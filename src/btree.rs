65c4bef7b296dc48587458613298bf709bac81aa
Michael Benfield <mike.benfield@gmail.com>
2018-09-21 15:19:54 -0700

    Some BTree comments

--- src/btree.rs
+++ src/btree.rs
@@ -1,13 +1,10 @@
-//! Simple implementation of B Trees. Refer to Cormen et al (2007).
+//! Simple implementation of B Trees. Refer to Cormen et al (2009).
 //!
 //! Like Cormen's description, but also maintains an index associated with each
-//! key. Currently delete is not implemented.
+//! key. Currently delete is not implemented. Also, this is just an in-memory B
+//! Tree, so I don't worry about disk reads and writes.
 
 use model::Model;
-
-trait Increment {
-    fn inc(self) -> Self;
-}
 
 const T: usize = 8;
 
@@ -22,6 +19,13 @@ struct BTreeNode<K, I> {
     children: u32,
 }
 
+/// A BTree, parametrized by key and index type.
+///
+/// The minimum degree is fixed at 8. I've made this a fixed constant rather
+/// than some sort of parameter because I want it to be optimized away rather
+/// than a runtime variable, and because Rust currently has issues with
+/// associated consts as array lengths (see rustc github issue #29646). It's
+/// possible to work around this but I'm not taking the time, at least for now.
 #[derive(Clone, Debug, Eq, PartialEq, Hash)]
 pub struct BTree<K, I> {
     nodes: Vec<BTreeNode<K, I>>,
@@ -49,13 +53,80 @@ where
     }
 }
 
+// Some notes on the private interface of this BTree:
+//
+// Nodes are identified by their `u32` index. Rather than calling a method to
+// retrieve a BTreeNode, methods are provided to retrieve features of a node
+// given the index. For instance, `fn keys(&self, node: u32) -> &[K]` retrieves
+// a slice of the keys associated with a given node.
 impl<K, I> BTree<K, I>
 where
     K: Copy + Default + PartialEq + PartialOrd,
     I: Copy + Default,
 {
+    /// Create a new BTree.
     pub fn new() -> Self {
         Default::default()
+    }
+
+    /// Find the index with this key that was inserted before any other index
+    /// with this key, or `None` if the key is not in the tree.
+    pub fn search(&self, key: K) -> Option<I> {
+        self.rsearch(self.root, key)
+    }
+
+    /// Insert `key` into the tree, mapping to `index`.
+    ///
+    /// As may be clear from the interface, no attempt is made to choose a
+    /// reasonable, ordered index - the caller is responsible.
+    pub fn insert(&mut self, key: K, index: I) {
+        let r = self.root;
+        if *self.key_count(r) == (2 * T - 1) as u32 {
+            let s = self.nodes.len() as u32;
+            self.root = s;
+            self.nodes.push(Default::default());
+            self.nodes[s as usize].children = self.children.len() as u32;
+            self.children.push(Default::default());
+            self.children_mut(s).unwrap()[0] = r;
+            self.split_child(s, 0);
+            self.insert_nonfull(s, key, index);
+        } else {
+            self.insert_nonfull(r, key, index);
+        }
+    }
+
+    fn split_child(&mut self, x: u32, i: usize) {
+        let z = self.nodes.len() as u32;
+        self.nodes.push(Default::default());
+        let y = self.children(x).expect("No children")[i];
+        *self.key_count_mut(z) = (T - 1) as u32;
+        for j in 0..T - 1 {
+            self.keys_mut(z)[j] = self.keys(y)[j + T];
+            self.indices_mut(z)[j] = self.indices(y)[j + T];
+        }
+        if let None = self.children(y) {
+            self.nodes[z as usize].children = 0xFFFFFFFF;
+        } else {
+            self.nodes[z as usize].children = self.children.len() as u32;
+            self.children.push(Default::default());
+            for j in 0..T {
+                self.children_mut(z).unwrap()[j] = self.children(y).unwrap()[j + T];
+            }
+        }
+        *self.key_count_mut(y) = (T - 1) as u32;
+        for j in i + 1..*self.key_count(x) as usize + 1 {
+            let j0 = *self.key_count(x) as usize + 1 - j;
+            let array = self.children_mut(x).unwrap();
+            array[j0 + 1] = array[j0];
+        }
+        self.children_mut(x).unwrap()[i + 1] = z;
+        for j in i..*self.key_count(x) as usize {
+            self.keys_mut(x)[j + 1] = self.keys(x)[j];
+            self.indices_mut(x)[j + 1] = self.indices(x)[j];
+        }
+        self.keys_mut(x)[i] = self.keys(y)[T - 1];
+        self.indices_mut(x)[i] = self.indices(y)[T - 1];
+        *self.key_count_mut(x) += 1;
     }
 
     fn keys(&self, node: u32) -> &[K] {
@@ -117,60 +188,6 @@ where
         match self.children(node) {
             None => None,
             Some(c) => self.rsearch(c[*self.key_count(node) as usize], key),
-        }
-    }
-
-    pub fn search(&self, key: K) -> Option<I> {
-        self.rsearch(self.root, key)
-    }
-
-    pub fn split_child(&mut self, x: u32, i: usize) {
-        let z = self.nodes.len() as u32;
-        self.nodes.push(Default::default());
-        let y = self.children(x).expect("No children")[i];
-        *self.key_count_mut(z) = (T - 1) as u32;
-        for j in 0..T - 1 {
-            self.keys_mut(z)[j] = self.keys(y)[j + T];
-            self.indices_mut(z)[j] = self.indices(y)[j + T];
-        }
-        if let None = self.children(y) {
-            self.nodes[z as usize].children = 0xFFFFFFFF;
-        } else {
-            self.nodes[z as usize].children = self.children.len() as u32;
-            self.children.push(Default::default());
-            for j in 0..T {
-                self.children_mut(z).unwrap()[j] = self.children(y).unwrap()[j + T];
-            }
-        }
-        *self.key_count_mut(y) = (T - 1) as u32;
-        for j in i + 1..*self.key_count(x) as usize + 1 {
-            let j0 = *self.key_count(x) as usize + 1 - j;
-            let array = self.children_mut(x).unwrap();
-            array[j0 + 1] = array[j0];
-        }
-        self.children_mut(x).unwrap()[i + 1] = z;
-        for j in i..*self.key_count(x) as usize {
-            self.keys_mut(x)[j + 1] = self.keys(x)[j];
-            self.indices_mut(x)[j + 1] = self.indices(x)[j];
-        }
-        self.keys_mut(x)[i] = self.keys(y)[T - 1];
-        self.indices_mut(x)[i] = self.indices(y)[T - 1];
-        *self.key_count_mut(x) += 1;
-    }
-
-    pub fn insert(&mut self, key: K, index: I) {
-        let r = self.root;
-        if *self.key_count(r) == (2 * T - 1) as u32 {
-            let s = self.nodes.len() as u32;
-            self.root = s;
-            self.nodes.push(Default::default());
-            self.nodes[s as usize].children = self.children.len() as u32;
-            self.children.push(Default::default());
-            self.children_mut(s).unwrap()[0] = r;
-            self.split_child(s, 0);
-            self.insert_nonfull(s, key, index);
-        } else {
-            self.insert_nonfull(r, key, index);
         }
     }
 
